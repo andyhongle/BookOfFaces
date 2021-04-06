d630d74443dc6f6eec234d55b6b4b7993ec63bdb
AndyLe <andyhongles@gmail.com>
2021-04-06 09:46:45 -0700

    Added user table and model

---
+++ BookOfFaces/app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ApplicationRecord
+
+validates :username, :password_digest, :session_token, :first_name,
+          :last_name, presence: true
+validates :username, :session_token, uniqueness: true
+validates :password, length:  {minimum: 6, allow_nil: true }
+
+after_initialize :ensure_session_token
+
+attr_reader :password
+
+
+def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+        return user
+    else
+        nil
+    end
+end
+
+def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+end
+
+def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+end
+
+
+def reset_session_token!
+    self.session_token = SecureRandom.urlsafe.base64
+    self.save!
+    self.session_token
+end
+
+def ensure_session_token! 
+    self.session_token ||= SecureRandom.urlsafe.base64
+end
+
+
+end # class end
+
