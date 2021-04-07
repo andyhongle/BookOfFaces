7c9beacf961087cf41398dfc470df60cda5c6cda
AndyLe <andyhongles@gmail.com>
2021-04-07 09:37:08 -0700

    Set up initial root and app

--- frontend/bookoffaces.jsx
+++ frontend/bookoffaces.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import Root from "./components/root";
+import Root from "./components/root";
 import configureStore from "./store/store";
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
     const store = configureStore();
+    // Test start
     window.getState = store.getState;
     window.dispatch = store.dispatch
-    // ReactDOM.render(<Root store={store} />, root)
+    // Test end
+    
+    ReactDOM.render(<Root store={store} />, root)
 })
