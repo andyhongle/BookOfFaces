0e529b670a4824c0256f26449e1236a0c405138a
AndyLe <andyhongles@gmail.com>
2021-04-06 17:25:32 -0700

    Test

---
+++ db/migrate/20210405201542_create_users.rb
@@ -0,0 +1,21 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :birthday
+      t.string :bio
+      t.string :education
+      t.string :hometown
+      t.string :relationship
+      t.string :websites
+      t.string :social_links
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
