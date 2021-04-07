bb8c07715a8f3402102bc4ab08993f53dc2431a9
AndyLe <andyhongles@gmail.com>
2021-04-06 18:24:38 -0700

    Can create, login, logout user

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,7 +7,7 @@ class Api::SessionsController < ApplicationController
             login!(@user)
             render '/api/users/show'
         else
-            render json: ["Invalid username/password combination"], status 401
+            render json: ["Invalid username/password combination"], status: 401
         end
     end
 
@@ -16,7 +16,7 @@ class Api::SessionsController < ApplicationController
             log_out!
             render json: {}
         else
-            render json: ["Nobody signed in"], status 404
+            render json: ["Nobody signed in"], status: 404
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do 
     resources :users, only: [:create, :update, :destroy]
+    resource :session, only: [:create, :destroy]
   end
 end
